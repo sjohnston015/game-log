1cf47de4dac34e651c631662392ceb13bdce310a
Seth Johnston <sjohnston@seths-macbook.local>
2025-06-20 11:18:03 -0400

    init commit

---
+++ src/test/java/com/johnstondev/gamelog/GameLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnstondev.gamelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
