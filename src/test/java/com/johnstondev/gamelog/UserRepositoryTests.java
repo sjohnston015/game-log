fce628da2d836f5dc41b2c205044c91b484dbdec
Seth Johnston <sjohnston@seths-macbook.local>
2025-06-21 14:08:01 -0400

    added user repo tests for TDD

--- src/test/java/com/johnstondev/gamelog/UserRepositoryTests.java
+++ src/test/java/com/johnstondev/gamelog/UserRepositoryTests.java
@@ -1,7 +1,42 @@
 package com.johnstondev.gamelog;
 
+import com.johnstondev.gamelog.model.User;
+import com.johnstondev.gamelog.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 public class UserRepositoryTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void saveAndFindUser() {
+
+        // create user, then save user
+        User user = new User(1L, "user", "test@email.com");
+        User saved = userRepository.save(user);
+
+        // check if user was saved
+        assertEquals(1L, saved.getId());
+        assertEquals("user", saved.getUsername());
+        assertEquals("test@email.com", saved.getEmail());
+
+        // check if user was actually saved and can be found
+        Optional<User> found = userRepository.findById(saved.getId());
+        assertTrue(found.isPresent());
+        assertEquals("user", found.get().getUsername());
+    }
+
 }
