93cfed70061505d5af8bbb82c9cc371f7ff0f56f
Seth Johnston <sjohnston@seths-macbook.local>
2025-06-21 14:18:59 -0400

    still failing user repo test

--- src/test/java/com/johnstondev/gamelog/UserRepositoryTests.java
+++ src/test/java/com/johnstondev/gamelog/UserRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
+@ActiveProfiles("test") // please work...
 public class UserRepositoryTests {
 
     @Autowired
