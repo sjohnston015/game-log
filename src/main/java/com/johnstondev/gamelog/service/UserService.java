92794aed716591fc46eb6ffe494718796acd7da8
Seth Johnston <sjohnston@seths-macbook.local>
2025-06-22 14:18:12 -0400

    UserService tests pass correctly

--- src/main/java/com/johnstondev/gamelog/service/UserService.java
+++ src/main/java/com/johnstondev/gamelog/service/UserService.java
@@ -2,6 +2,7 @@ package com.johnstondev.gamelog.service;
 
 import com.johnstondev.gamelog.model.User;
 import com.johnstondev.gamelog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,6 +13,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     // constructor injection
+    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
