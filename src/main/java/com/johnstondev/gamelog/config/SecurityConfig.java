220b32ca08424b20d2e84176aa1291a2bafce1da
Seth Johnston <sjohnston@seths-macbook.local>
2025-06-25 16:45:36 -0400

    disabled auth while developing

---
+++ src/main/java/com/johnstondev/gamelog/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.johnstondev.gamelog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()  // allow all requests without authentication for now...
+                )
+                .csrf(AbstractHttpConfigurer::disable);  // disable CSRF for API testing
+
+        return http.build();
+    }
+}
